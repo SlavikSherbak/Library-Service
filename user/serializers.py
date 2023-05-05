e88e9937cce39bd73d8ed5ad8a47a3ad907e78ad
Viacheslav Shcherbak <32074944+SlavikSherbak@users.noreply.github.com>
2023-05-05 15:11:54 +0300

    feat some fix serializers & views

--- user/serializers.py
+++ user/serializers.py
@@ -1,14 +1,12 @@
 from django.contrib.auth import get_user_model
-from django.utils.text import gettext_lazy as _
 from rest_framework import serializers
-from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ("id", "email", "password", "is_staff")
-        read_only_fields = ("id", "is_staff",)
+        read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
@@ -24,21 +22,3 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
-
-
-class RefreshTokenSerializer(serializers.Serializer):
-    refresh = serializers.CharField()
-
-    default_error_messages = {
-        'bad_token': _('Token is invalid or expired')
-    }
-
-    def validate(self, attrs):
-        self.token = attrs['refresh']
-        return attrs
-
-    def save(self, **kwargs):
-        try:
-            RefreshToken(self.token).blacklist()
-        except TokenError:
-            self.fail('bad_token')
