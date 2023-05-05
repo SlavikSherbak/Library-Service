0df6108aaf2acbcc61dd096a9e0edd0db93014e0
Viacheslav Shcherbak <32074944+SlavikSherbak@users.noreply.github.com>
2023-05-05 11:50:05 +0300

    added views for user model

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from user.serializers import UserSerializer, RefreshTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
