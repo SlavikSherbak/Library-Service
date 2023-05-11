474f44e5cb9f01590900fab4b09343086a2e3f9d
Viacheslav Shcherbak <32074944+SlavikSherbak@users.noreply.github.com>
2023-05-11 15:23:49 +0300

    added model book

--- book/models.py
+++ book/models.py
@@ -1,3 +1,26 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    HARD = "HA"
+    SOFT = "SO"
+    COVER_CHOICES = [
+        (HARD, "Hard"),
+        (SOFT, "Soft"),
+    ]
+    title = models.CharField(max_length=255, unique=True)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=2,
+        choices=COVER_CHOICES,
+        default=SOFT,
+    )
+    inventory = models.IntegerField(validators=MinValueValidator(1))
+    daily_fee = models.FloatField()
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return f"{self.title} (authors: {self.author})"
