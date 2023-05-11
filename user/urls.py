e96e48715a0b842b375f1fc50734f6055c0e8de1
Viacheslav Shcherbak <32074944+SlavikSherbak@users.noreply.github.com>
2023-05-11 13:39:20 +0300

    add user app urls & register app in project urls list

--- user/urls.py
+++ user/urls.py
@@ -10,9 +10,9 @@ from user.views import CreateUserView, ManageUserView
 app_name = "user"
 
 urlpatterns = [
-    path("users/", CreateUserView.as_view(), name="create"),
-    path("users/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("users/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("", CreateUserView.as_view(), name="create"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
