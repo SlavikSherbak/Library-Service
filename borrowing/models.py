30a90f8e29800e2b97fe0a99b9d7caf33ad4e604
Viacheslav Shcherbak <32074944+SlavikSherbak@users.noreply.github.com>
2023-05-15 16:53:13 +0300

    add model Borrowing & register it on admin.py

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+import book.models
+import user.models
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
+    book_id = models.ManyToManyField(book.models.Book)
+    user_id = models.ForeignKey(user.models.User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Borrowing {self.book_id.title} expected return date {self.expected_return_date}"
+
+    class Meta:
+        ordering = ["borrow_date"]
